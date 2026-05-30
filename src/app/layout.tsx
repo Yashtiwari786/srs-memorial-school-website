530d887331e6b64c0277710f56900d5c11a322f5
harshvardhantiwari <tiwaiharsh01678@gmail.com>
2026-05-30 13:03:40 +0530

    Merge remote main into local main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "S.R.S. Memorial Kids Garden School | Learning with Joy, Growing with Values",
